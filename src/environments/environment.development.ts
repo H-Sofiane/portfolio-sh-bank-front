f330187f8c901c2a81ac6edfc589a054d026ea26
H-Sofiane <haddad-sofiane@live.fr>
2025-04-10 17:57:22 +0200

    Auto-generated commit

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8085',
-  GOOGLE_CLIENT_ID: '',
-  GOOGLE_CLIENT_SECRET: '',
-  redirectUri: "http://localhost:4200"
-};
--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'http://localhost:8085',
-    GOOGLE_CLIENT_ID: '',
-    GOOGLE_CLIENT_SECRET: '',
-    redirectUri: "http://localhost:4200"
-  };
-  
