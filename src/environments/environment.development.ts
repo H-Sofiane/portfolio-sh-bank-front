2ebdf181377f35cefe25b4e2c8a75e6121fcf292
H-Sofiane <haddad-sofiane@live.fr>
2025-04-10 16:46:32 +0200

    Clean commit: without any secrets

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8085',
+  GOOGLE_CLIENT_ID: '',
+  GOOGLE_CLIENT_SECRET: '',
+  redirectUri: "http://localhost:4200"
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://localhost:8085',
+    GOOGLE_CLIENT_ID: '',
+    GOOGLE_CLIENT_SECRET: '',
+    redirectUri: "http://localhost:4200"
+  };
+  
